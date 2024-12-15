e127dbbbd8b00f2982aee4b1a287078d246bf6b8
AndyVN2000 <andyvu@outlook.dk>
2024-12-15 14:30:06 +0100

    More notes on computing multiplication triplets

--- multiplicationTriple.py
+++ multiplicationTriple.py
@@ -38,10 +38,19 @@ class MultiplicationTriple:
     
 
 """
+Relevant resources:
+- MZ17 page 6 under 'Vectorization in the Shared Setting' written in bold and down to page 7 until 'Arithmetic Operations on Shared Decimal Numbers' written in bold
+- MZ17 page 8
+
+Multiplication triples serves the purpose of computing the multiplication of two matrices A and B in a secret sharing. The triples are U, V and Z.
+In other words, two parties wants to compute [A] x [B] that are secret shared. Thus a dealer must compute [U], [V] and [Z], where U has same dim as A and
+V has same dim as B.
+
 According to page 8 of MZ17, I deduce that matrix U has dimensions n x d, where n must be the number of samples and d must be the number of features. 
 I can also deduce that the matrix V has d rows, but number of columns is unknown.
+Elements of U and V are uniformly random in the group Z_{2^l}
 A |B| x d submatrix of U is defined and called A, where |B| is the number of batches for stochastic gradient descent.
-Finally, the paper aims to find the shares of C = A x B. A, B and C are equivalent to the triples u,v and w from BeDOZa respectively.
+Finally, the paper aims to find the shares of C = A x B and repeat it such that they compute Z. A, B and Z are equivalent to the triples u,v and w from BeDOZa respectively.
 
 There are two ways to compute the multiplication triples which MZ17 calls the offline phase: linear homomorphic encryption or oblivious transfer.
 """
@@ -52,6 +61,26 @@ Linear homomorphic encryption
 2. Take a submatrix A out of U (random???)
 3. Take a column B (Bad naming, but this is how MZ17 names them) out of V (random???)
 4. Compute C such that C = [A]_0 x [B]_0 + [A]_0 x [B]_1 + [A]_1 x [B]_0 + [A]_1 x [B]_1
+5. Compute [A]_0 x [B]_1
+6. Compute [A]_1 x [B]_0
+7. Remaining terms are computed locally without communication between parties.
 """
 
+def mult_triples(n, d, t):
+    """ Computes the multiplication triplets
+    
+    Parameters
+    ----------
+    n : int
+        Number of rows/data samples
+    d : int
+        Number of columns/
+    t : int
+        Number of mini-batches
+
+    Returns
+    -------
+    tuple
+        the shared triplets [U], [V], [Z], [V'], [Z']
+    """
 
