fa189e8e1a69dd98c349c1370dc801283a8e1a83
AndyVN2000 <andyvu@outlook.dk>
2025-01-03 17:08:21 +0100

    Initial commit for fixed point arithmetic

---
+++ fixed_point_arithmetic.py
@@ -0,0 +1,20 @@
+
+def fixed_point_mult(x, y, l):
+    """ Performs multiplication in fixed point arithmetic between two decimal numbers x and y
+    
+    Parameters
+    ----------
+    x : float
+        Decimal number
+    y : float
+        Decimal number
+    l : int
+        Bit length of fractional part of decimal numbers. Determines the scaling factor f^l
+    """
+
+    # TODO: Scale x and y by using the scaling factor
+
+    # TODO: Perform multiplication on the scaled numbers
+
+    # TODO: Truncate the resulting product and return
+    pass
